3c4b133d2c1e2258f3a7bf568d43a11befa02a2f
Andrey E. Antipov <Andrey.E.Antipov@gmail.com>
2016-10-11 11:52:08 -0700

    rearrange and clean repo

--- clusters/pbs_stampede.py
+++ clusters/pbs_stampede.py
@@ -19,30 +19,28 @@ def which(file):
 def njobs(name = ""):
     """ Wrapper around qstat - gets the total number of tasks with a given name or account """ 
     user = os.environ.get('USER')
-    ntasks = int(subprocess.check_output("qstat | grep "+user+" | grep '"+name+"' | grep ' R ' | wc -l", shell=True))
+    ntasks = int(subprocess.check_output("squeue -u "+user+" | grep "+user+" | grep '"+name+"' | wc -l", shell=True))
     return ntasks
 
 # Submit mpi job
 def submit_mpi(   commands,  # commands to submit
                   name, # name of the job
                   nprocs, # number of cpus
-                  account, # which account to use
-                  queue, # queue name
+                  queue = "normal", # queue name
                   prefix = "", # what will be executed before the command. Smth like "export LD_LIBRARY_PATH="..."
                   postfix = "", # what will be executed after the run
                   add_mpirun = False, # add mpirun in front
                   add_mpirun_each = False, # if multiple commands - add mpirun in front of each of them
-                  mpi_exec = "mpirun", # which mpirun command
+                  mpi_exec = "ibrun", # which mpirun command
                   use_scratch = False, # work in scratch
                   scratch_dir = os.environ.get("SCRATCH"), # location of scratch fs 
-                  job_input = "*.ini *.dat *.h5 *.sh", # what to copy to scratch as input
+                  job_input = "*.ini *.dat *.h5", # what to copy to scratch as input
                   output_stream_file = "stdout", # where to gather output 
                   error_stream_file = "stderr", # where to gather errors
                   cpu_time = "164:00:00", # how long to run
                   ram_size = "4096mb", # memory allocation
                   file_size="4096mb",
                   pbs_file = "task.pbs", # name of the pbs file 
-                  priority = 0,
                   dry_run = False ):
     linesep = '\n' # see https://docs.python.org/2/library/os.html?highlight=linesep#os.linesep
     scratch_dir = os.getcwd() if scratch_dir==None else scratch_dir
@@ -53,7 +51,7 @@ def submit_mpi(   commands,  # commands to submit
     if isinstance(commands[0][0], list):
         raise NameError('Commands should be a list with depth <=2') 
     # By now we have a 2d array of commands. Convert them to '\n' separated commands
-    print "Submitting :", linesep.join([' '.join(x) for x in commands]), "to account",account
+    print "Submitting :", linesep.join([' '.join(x) for x in commands]), "to queue",queue
     start_dir = os.getcwd()
     scratch_location = scratch_dir+os.path.sep
     scratch_location = scratch_location + time.strftime("%Y-%m-%d:%H:%M:%S", time.gmtime())
@@ -68,19 +66,16 @@ def submit_mpi(   commands,  # commands to submit
         os.remove(error_stream_file)
     f = open(pbs_file,"w")
     f.write("#!"+bash_path+linesep)
-    f.write("#PBS -S "+bash_path+linesep) # load bash
-    f.write("#PBS -N "+name+linesep) # job name
-    f.write("#PBS -A "+account+linesep) # account
-    f.write("#PBS -q "+queue+linesep) # queue
-    f.write("#PBS -m n"+linesep) # silence emails
-    f.write("#PBS -l qos=flux"+linesep)
-    f.write("#PBS -l procs="+str(nprocs)+",walltime="+cpu_time+linesep)
-    f.write("#PBS -l pmem="+ram_size+linesep)
-    f.write("#PBS -p "+str(priority)+linesep)
-    f.write("#PBS -o "+os.path.join(start_dir, output_stream_file)+linesep) #output
-    f.write("#PBS -e "+os.path.join(start_dir, error_stream_file)+linesep) #errors
-    #f.write("#PBS -k oe" + linesep)
-    f.write("#PBS -V "+linesep) # Takes your current environment (like PATH and other variables) and sends them along with your job to the compute node.
+    #f.write("#SBATCH -S "+bash_path+linesep) # load bash
+    f.write("#SBATCH -J "+name+linesep) # job name
+    #f.write("#SBATCH -A "+account+linesep) # account
+    f.write("#SBATCH -p "+queue+linesep) # queue
+    f.write("#SBATCH -n "+str(nprocs) + linesep)
+    f.write("#SBATCH -t "+cpu_time+linesep)
+    f.write("#SBATCH --mail-type=fail"+linesep)
+    f.write("#SBATCH -o "+os.path.join(start_dir, output_stream_file)+linesep) #output
+    f.write("#SBATCH -e "+os.path.join(start_dir, error_stream_file)+linesep) #errors
+    #f.write("#SBATCH -V "+linesep) # Takes your current environment (like PATH and other variables) and sends them along with your job to the compute node.
     f.write(linesep)
     f.write("cd "+start_dir+linesep)
     if use_scratch:
@@ -88,13 +83,15 @@ def submit_mpi(   commands,  # commands to submit
         f.write("cp -v "+job_input+" "+scratch_location+os.path.sep+linesep)
         f.write("cd "+scratch_location+linesep)
 
-    f.write("cat $PBS_NODEFILE" + linesep)
+    f.write("echo nodes : $SLURM_NODELIST" + linesep)
+    f.write("echo queue : $SLURM_QUEUE" + linesep)
+    f.write("echo account : $SLURM_TACC_ACCOUNT" + linesep)
     f.write(prefix + linesep)
 
     call_str = ""
     if add_mpirun:
         mpi_exec = which(mpi_exec)
-        mpi_str = mpi_exec + " --np " + str(nprocs)+" "
+        mpi_str = mpi_exec + " -n " + str(nprocs)+" -o 0 "
         if add_mpirun_each:
             call_str = mpi_str + (linesep + mpi_str).join([' '.join(x) for x in commands])
         else:
@@ -109,8 +106,8 @@ def submit_mpi(   commands,  # commands to submit
         f.write("rm -rfv "+scratch_location+linesep)
     f.close()
     
-    qsub = which("qsub")
-    full_command=qsub+" "+pbs_file
+    sbatch = which("sbatch")
+    full_command=sbatch+" "+pbs_file
 
     print full_command
     if not dry_run:
@@ -119,8 +116,7 @@ def submit_mpi(   commands,  # commands to submit
 # Submit serial job
 def submit_serial(commands,  # commands to submit
                   name, # name of the job
-                  account, # which account to use
-                  queue, # queue name
+                  queue = "serial", 
                   prefix = "", # what will be executed before the command. Smth like "export LD_LIBRARY_PATH="..."
                   postfix = "", # what will be executed after the run
                   use_scratch = False, # work in scratch
@@ -138,7 +134,6 @@ def submit_serial(commands,  # commands to submit
     submit_mpi(commands = commands, 
                name = name, 
                nprocs = 1, 
-               account = account, 
                queue = queue, 
                add_mpirun = False,
                prefix = prefix, 
@@ -151,7 +146,6 @@ def submit_serial(commands,  # commands to submit
                use_scratch = use_scratch, 
                scratch_dir = scratch_dir,
                pbs_file = pbs_file,
-               priority = 0,
                dry_run = dry_run)
 
 
